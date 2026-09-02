425a9b031880841db629b3cca4c965395a0f205c
Alen <rightintellectual@gmail.com>
2026-09-02 12:23:58 +0400

    pdf update

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,36 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/gvn-paul-nyongesa-otuoma',
+        destination: '/gvn-paul-nyongesa-otuoma.pdf.pdf',
+      },
+      {
+        source: '/gvn-paul-nyongesa-otuoma/',
+        destination: '/gvn-paul-nyongesa-otuoma.pdf.pdf',
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/gvn-paul-nyongesa-otuoma',
+        headers: [
+          { key: 'Content-Type', value: 'application/pdf' },
+          { key: 'Content-Disposition', value: 'inline' },
+        ],
+      },
+      {
+        source: '/gvn-paul-nyongesa-otuoma/',
+        headers: [
+          { key: 'Content-Type', value: 'application/pdf' },
+          { key: 'Content-Disposition', value: 'inline' },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
